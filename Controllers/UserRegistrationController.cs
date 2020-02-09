43379b295faf65a79a566c664bb7fe4693c1d9fe
Krzysztof Pietrasik <43923921+KrzysztofPietrasik@users.noreply.github.com>
2020-02-09 21:42:47 +0100

    Dodana rejestracja

---
+++ Controllers/UserRegistrationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Travel_Manager.Models;
+
+namespace Travel_Manager.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        private readonly ApplicationUserClass _auc;
+
+        public UserRegistrationController(ApplicationUserClass auc)
+        {
+            _auc = auc;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(UserClass uc)
+        {
+            _auc.Add(uc);
+            _auc.SaveChanges();
+            ViewBag.message = "The User " + uc.Username + "is saved successfully";
+            return View();
+        }
+    }
+}
